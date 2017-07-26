1055c9742dfba39b97e34580c68c7fc66088c566
Yubraj Pokharel <yubrajpokharel@HeartBleed.local>
2017-07-26 01:42:57 -0500

    consumers by group ID

---
+++ src/main/java/com/yubraj/consumerGroup/Consumer2.java
@@ -0,0 +1,41 @@
+package com.yubraj.consumerGroup;
+
+import org.apache.kafka.clients.consumer.ConsumerRecord;
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.clients.consumer.KafkaConsumer;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+/**
+ * Created by yubrajpokharel on 7/10/17.
+ */
+public class Consumer2 {
+    public static void main(String[] args) {
+        Properties properties = new Properties();
+        properties.put("bootstrap.servers", "localhost:9092");
+        properties.put("key.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("value.deserializer", "org.apache.kafka.common.serialization.StringDeserializer");
+        properties.put("group.id", "myGroup");
+
+        KafkaConsumer<String , String > consumer = new KafkaConsumer<String, String>(properties);
+        List<String > myTopics = new ArrayList<String>();
+        myTopics.add("my-first-topic");
+
+        consumer.subscribe(myTopics);
+
+        try{
+            while (true){
+                ConsumerRecords<String, String> consumerRecord = consumer.poll(10);
+                for(ConsumerRecord<String, String> record: consumerRecord){
+                    System.out.println(record.toString());
+                }
+            }
+        }catch (Exception e){
+            e.printStackTrace();
+        }finally {
+            consumer.close();
+        }
+    }
+}
